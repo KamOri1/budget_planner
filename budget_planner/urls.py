0ea394679952d947f5fadcd40dfae11fe19dab8f
KamOri1 <iknowthis45@gmail.com>
2024-12-06 23:58:16 +0100

    feat: add billing.html

--- budget_planner/urls.py
+++ budget_planner/urls.py
@@ -17,12 +17,12 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
-from dashboard.views import dashboard, base
+from dashboard.views import dashboard, billing
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", dashboard),
-    path("base", base),
+    path("billing", billing),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- dashboard/views.py
+++ dashboard/views.py
@@ -5,5 +5,9 @@ def dashboard(request):
     return render(request=request, template_name="dashboard.html")
 
 
+def billing(request):
+    return render(request=request, template_name="billing.html")
+
+
 def base(request):
     return render(request=request, template_name="base.html")
