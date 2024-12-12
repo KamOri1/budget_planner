5ed151380e2d8e8811e4b991c8eeb8158df9377b
KamOri1 <iknowthis45@gmail.com>
2024-12-12 23:56:52 +0100

    feat: add django form to register user

--- users/urls.py
+++ users/urls.py
@@ -1,13 +1,11 @@
-from django.conf import settings
-from django.conf.urls.static import static
-from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path
 
 from users import views as user_views
 
 urlpatterns = [
-    path("sign-up", user_views.register, name="register"),
+    # path("register", user_views.register, name="register"),
+    path("register/", user_views.RegisterView.as_view(), name="register"),
     path(
         "login",
         auth_views.LoginView.as_view(template_name="users/login.html"),
--- users/views.py
+++ users/views.py
@@ -1,27 +1,50 @@
 from django.contrib import messages
 from django.contrib.auth import logout
 from django.shortcuts import redirect, render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
 from .forms import UserRegisterForm
 
 
-def register(request):  # TODO Class Based View + Template z boostrap'a.
-    if request.method == "POST":
-        form = UserRegisterForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get("username")
-            messages.success(
-                request, f"Dear {username}, you been successfully sign up."
-            )
-            return redirect("/login")
-    else:
-        form = UserRegisterForm()
+class RegisterView(CreateView):
+    form_class = UserRegisterForm
+    template_name = "users/register.html"
+    success_url = reverse_lazy("login")
 
-    return render(request, "users/sign-up.html", {"form": form})
+    def form_valid(self, form, request):
+        if request.method == "POST":
+            form = UserRegisterForm(request.POST)
+            if form.is_valid():
+                form.save()
+                username = form.cleaned_data.get("username")
+                messages.success(
+                    request, f"Dear {username}, you been successfully sign up."
+                )
+                return redirect("/login")
+        else:
+            form = UserRegisterForm()
+
+        return render(request, "users/register.html", {"form": form})
+
+
+# def register(request):  # TODO Class Based View + Template z boostrap'a.
+#     if request.method == "POST":
+#         form = UserRegisterForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             username = form.cleaned_data.get("username")
+#             messages.success(
+#                 request, f"Dear {username}, you been successfully sign up."
+#             )
+#             return redirect("/login")
+#     else:
+#         form = UserRegisterForm()
+#
+#     return render(request, "users/register.html", {"form": form})
 
 
 def logout_user(request):
     logout(request)
     messages.success(request, "you been log out.")
-    return redirect("/login")
+    return redirect("login")
