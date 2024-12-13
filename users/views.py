981964f0327643226997b4a43dae1dbcaf6fe5b0
KamOri1 <iknowthis45@gmail.com>
2024-12-13 23:43:21 +0100

    feat: start app wallet

--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
 from django.contrib import messages
 from django.contrib.auth import logout
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
@@ -12,20 +12,10 @@ class RegisterView(CreateView):
     template_name = "users/register.html"
     success_url = reverse_lazy("login")
 
-    def form_valid(self, form, request):
-        if request.method == "POST":
-            form = UserRegisterForm(request.POST)
-            if form.is_valid():
-                form.save()
-                username = form.cleaned_data.get("username")
-                messages.success(
-                    request, f"Dear {username}, you been successfully sign up."
-                )
-                return redirect("/login")
-        else:
-            form = UserRegisterForm()
-
-        return render(request, "users/register.html", {"form": form})
+    def form_valid(self, form):
+        user = form.save()
+        messages.success(self.request, f"Account created for {user.username}")
+        return super().form_valid(form)
 
 
 # def register(request):  # TODO Class Based View + Template z boostrap'a.
