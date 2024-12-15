e75d15c4e2f2ee4907639bb45cccc9732ffc7d65
KamOri1 <iknowthis45@gmail.com>
2024-12-15 22:27:05 +0100

    feat(models): add wallet model

--- wallet/models.py
+++ wallet/models.py
@@ -1,1 +1,8 @@
-# Create your models here.
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Wallet(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    wallet_name = models.CharField(max_length=100)
+    portfolio_value = models.IntegerField()
