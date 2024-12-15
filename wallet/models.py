fb6e949b12587723341e5ecb23ca8650cc14da0b
KamOri1 <iknowthis45@gmail.com>
2024-12-15 22:32:12 +0100

    feat(models): add BankAccount model

--- wallet/models.py
+++ wallet/models.py
@@ -6,3 +6,10 @@ class Wallet(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     wallet_name = models.CharField(max_length=100)
     portfolio_value = models.IntegerField()
+
+
+class BankAccount(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    account_name = models.CharField(max_length=100)
+    account_number = models.IntegerField()
+    sum_of_funds = models.IntegerField()
