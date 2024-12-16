eaee5332abdb70d5949a8bee3086a6b1a5d3babd
KamOri1 <iknowthis45@gmail.com>
2024-12-16 20:10:46 +0100

    feat: added CRUD  DRF for wallet app

--- wallet/urls.py
+++ wallet/urls.py
@@ -1,8 +1,23 @@
 from django.urls import path
 
-from .views import BankAccountViewSet, WalletViewSet
+from .views import (
+    BankAccountViewSet,
+    DeleteBankAccount,
+    DeleteWallet,
+    GetAllBankAccount,
+    GetAllWallet,
+    WalletViewSet,
+)
 
 urlpatterns = [
     path("add/wallet", WalletViewSet.as_view(), name="create_wallet"),
+    path("list/wallet", GetAllWallet.as_view(), name="list_wallet"),
+    path("delete/wallet/<int:pk>/", DeleteWallet.as_view(), name="delete_wallet"),
     path("add/bank_account", BankAccountViewSet.as_view(), name="create_bank_account"),
+    path("list/bank_account", GetAllBankAccount.as_view(), name="list_bank_account"),
+    path(
+        "delete/bank_account/<int:pk>/",
+        DeleteBankAccount.as_view(),
+        name="delete_bank_account",
+    ),
 ]
--- wallet/views.py
+++ wallet/views.py
@@ -1,3 +1,4 @@
+from rest_framework import permissions
 from rest_framework.viewsets import generics
 
 from wallet.models import BankAccount, Wallet
@@ -10,6 +11,28 @@ class WalletViewSet(generics.CreateAPIView):
     queryset = Wallet.objects.all()
 
 
+class GetAllWallet(generics.ListAPIView):
+    serializer_class = WalletSerializer
+    queryset = Wallet.objects.all()
+
+
+class DeleteWallet(generics.DestroyAPIView):
+    serializer_class = WalletSerializer
+    queryset = Wallet.objects.all()
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
 class BankAccountViewSet(generics.CreateAPIView):
     serializer_class = BankAccountSerializer
     queryset = BankAccount.objects.all()
+
+
+class GetAllBankAccount(generics.ListAPIView):
+    serializer_class = BankAccountSerializer
+    queryset = BankAccount.objects.all()
+
+
+class DeleteBankAccount(generics.DestroyAPIView):
+    serializer_class = BankAccountSerializer
+    queryset = BankAccount.objects.all()
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
