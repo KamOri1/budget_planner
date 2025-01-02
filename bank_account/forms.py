7911c02f529f012c037169e3b69c9fe95fb903e6
KamOri1 <iknowthis45@gmail.com>
2025-01-02 15:20:17 +0100

    feat(froms): add Created form

---
+++ bank_account/forms.py
@@ -0,0 +1,28 @@
+from django import forms  # noqa
+
+from .models import BankAccount
+
+
+class CreateAccountForm(forms.Form):
+    account_name = forms.CharField(
+        label="Account name",
+        widget=forms.TextInput(
+            attrs={"class": "form-control", "placeholder": "Account name"}
+        ),
+    )
+    account_number = forms.IntegerField(
+        label="Account number",
+        widget=forms.NumberInput(
+            attrs={"class": "form-control", "placeholder": "Account number"}
+        ),
+    )
+    sum_of_funds = forms.IntegerField(
+        label="Sum of funds",
+        widget=forms.NumberInput(
+            attrs={"class": "form-control", "placeholder": "Sum of funds"}
+        ),
+    )
+
+    class Meta:
+        model = BankAccount
+        fields = ["account_name", "account_number", "sum_of_funds"]
