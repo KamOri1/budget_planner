6bed5ecafe7deaf81551b0e892d1e6e356a97c6d
KamOri1 <iknowthis45@gmail.com>
2025-01-02 17:17:35 +0100

    refactor(models): adding field to model Transactions and Category

--- transaction/models.py
+++ transaction/models.py
@@ -5,11 +5,19 @@ from django.db import models
 class Category(models.Model):
     category_name = models.CharField(max_length=100)
     category_type = models.CharField(max_length=100)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.category_name
 
 
 class Transaction(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
     sum_amount = models.FloatField()
     description = models.TextField()
+    transaction_name = models.TextField(default="")
     transaction_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.transaction_name
