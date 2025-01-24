23a5acc331c38b023e6828b27628fd4b2b98ed9f
KamOri1 <iknowthis45@gmail.com>
2025-01-24 23:54:26 +0100

    feat(todays_money): added new function to compare money with previos mont in dashboard app

--- dashboard/utils/today_money.py
+++ dashboard/utils/today_money.py
@@ -64,3 +64,66 @@ class TodayMoney:
                 (current_month_profit - previous_month_profit) / previous_month_profit
             ) * 100
             return round(profit_percentage_change)
+
+    def compare_monthly_expenses(self) -> float:
+        current_month_expenses: float = self.sum_of_expenses()
+        previous_month_expenses: float = 0
+        current_month = datetime.now().strftime("%m")
+        current_years = datetime.now().strftime("%Y")
+
+        if int(current_month) == 1:
+            previous_month: str = "12"
+            previous_years: str = str(int(current_years) - 1)
+        else:
+            previous_month: str = str(int(current_month) - 1)
+            previous_years: str = current_years
+
+        for transaction in self.transactions:
+            if transaction.category_id in self.category.filter(category_type="cost"):
+                if (
+                    transaction.transaction_date.strftime("%Y-%m")
+                    == f"{previous_years}-{previous_month}"
+                ):
+                    previous_month_expenses += transaction.sum_amount
+
+        if previous_month_expenses == 0:
+            return 0
+        else:
+            expenses_percentage_change = (
+                (current_month_expenses - previous_month_expenses)
+                / previous_month_expenses
+            ) * 100
+            return round(expenses_percentage_change)
+
+    def compare_today_profit(self) -> float:
+        current_month_today_profit: float = self.sum_of_profit()
+        previous_month_today_profit: float = 0
+        current_month = datetime.now().strftime("%m")
+        current_years = datetime.now().strftime("%Y")
+
+        if int(current_month) == 1:
+            previous_month: str = "12"
+            previous_years: str = str(int(current_years) - 1)
+        else:
+            previous_month: str = str(int(current_month) - 1)
+            previous_years: str = current_years
+
+        for transaction in self.transactions:
+            if transaction.category_id in self.category.filter(category_type="profit"):
+                if transaction.transaction_date.strftime(
+                    "%Y-%m"
+                ) == f"{previous_years}-{previous_month}" and transaction.transaction_date.strftime(
+                    "%d"
+                ) <= datetime.now().strftime(
+                    "%d"
+                ):
+                    previous_month_today_profit += transaction.sum_amount
+
+        if previous_month_today_profit == 0:
+            return 0
+        else:
+            profit_percentage_change = (
+                (current_month_today_profit - previous_month_today_profit)
+                / previous_month_today_profit
+            ) * 100
+            return round(profit_percentage_change)
--- dashboard/views.py
+++ dashboard/views.py
@@ -21,7 +21,9 @@ def dashboard(request):
         "today_profit": today_profit.today_profit,
         "monthly_income": today_profit.sum_of_profit(),
         "monthly_expenses": today_profit.sum_of_expenses(),
-        "monthly_compare": today_profit.compare_monthly_profit(),
+        "monthly_income_compare": today_profit.compare_monthly_profit(),
+        "monthly_expenses_compare": today_profit.compare_monthly_expenses(),
+        "today_profit_compare": today_profit.compare_today_profit(),
     }
 
     return render(request=request, template_name="dashboard.html", context=context)
