48593e2d6fe7eaaf6de377cd21177e59595fb054
KamOri1 <iknowthis45@gmail.com>
2025-02-09 22:56:26 +0100

    test(bank_account): create test forBankAccount model

---
+++ bank_account/tests.py
@@ -0,0 +1,24 @@
+from django.contrib.auth.models import User
+from django.test import TestCase
+
+from bank_account.models import BankAccount
+
+
+class TestModels(TestCase):
+    def test_model_BankAccount(self):
+        user = User.objects.create_user(
+            username="Test", password="password", email="test@gmail.com"
+        )
+
+        bank_account = BankAccount.objects.create(
+            user_id=user,
+            account_name="Shopping",
+            account_number="102030",
+            sum_of_funds="144",
+        )
+
+        self.assertEqual(str(bank_account.user_id), user.username)
+        self.assertEqual(str(bank_account), "Shopping")
+        self.assertEqual(str(bank_account.account_number), "102030")
+        self.assertEqual(str(bank_account.sum_of_funds), "144")
+        self.assertTrue(isinstance(bank_account, BankAccount))
