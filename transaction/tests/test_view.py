e2ba69925e789251692f8a47da320fc0d41540c4
KamOri1 <iknowthis45@gmail.com>
2025-02-20 22:15:25 +0100

    test)transaction): Created transaction delete test

--- transaction/tests/test_view.py
+++ transaction/tests/test_view.py
@@ -11,7 +11,7 @@ from rest_framework.reverse import reverse_lazy
 
 from category.models import Category, CategoryType
 from transaction.models import Transaction
-from transaction.views import TransactionUpdateView
+from transaction.views import TransactionDeleteView, TransactionUpdateView
 
 
 class TestTransactionCreateView(TestCase):
@@ -121,7 +121,7 @@ class TestTransactionUpdateView(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "transaction/transaction_home_page.html")
 
-    def test_update_category(self):
+    def test_update_transaction(self):
         """Checks whether the selected transaction has been updated."""
 
         transaction = Transaction.objects.create(
@@ -150,3 +150,71 @@ class TestTransactionUpdateView(TestCase):
         self.assertEqual(response.status_code, 302)
         self.assertEqual(updated_transaction.description, "Another description")
         self.assertEqual(updated_transaction.sum_amount, 10.45)
+
+
+class TestCategoryDeleteView(TestCase):
+    """
+    Test cases for the Transaction delete view.
+    These tests verify the behavior of deleting an existing Transaction, correct redirections,
+    form handling, and database updates
+    """
+
+    def setUp(self):
+        """Set up test data."""
+        self.categoryType = CategoryType.objects.create(
+            type="profit",
+        )
+        self.success_url = reverse_lazy("transaction-home")
+        self.index_url = reverse_lazy("create_transaction")
+        self.client = Client()
+        self.user = User.objects.create_user(
+            username="Test1", password="password", email="test1@gmail.com"
+        )
+        self.client.force_login(self.user)
+        self.category = Category.objects.create(
+            user_id=self.user,
+            category_name="test",
+            category_type=self.categoryType,
+        )
+        self.transaction_date_value = datetime(
+            2025, 2, 10, 11, 35, 10, tzinfo=timezone.utc
+        )
+
+        self.transaction = Transaction.objects.create(
+            user_id=self.user,
+            category_id=self.category,
+            sum_amount=433.33,
+            description="something to test",
+            transaction_name="Shopping",
+            transaction_date=self.transaction_date_value,
+        )
+        self.request = HttpRequest()
+
+    def test_get_queryset(self):
+        """The test simulates user logging in and checks that only the categories he has added are available"""
+
+        self.request.user = self.user
+        view = TransactionDeleteView()
+        view.request = self.request
+        queryset = view.get_queryset()
+
+        expected_count = Transaction.objects.filter(user_id=self.user).count()
+
+        self.assertEqual(queryset.count(), expected_count)
+
+        for transaction in queryset:
+            self.assertEqual(transaction.user_id, self.user)
+
+    def test_delete_transaction(self):
+        """Checks whether the selected transaction has been deleted."""
+
+        self.client.force_login(self.user)
+        delete_url = reverse_lazy("delete_transaction", args=[self.transaction.pk])
+        response = self.client.post(delete_url)
+
+        self.assertEqual(response.status_code, 302)
+
+        with self.assertRaises(Transaction.DoesNotExist):
+            Transaction.objects.get(pk=self.transaction.pk)
+
+        self.assertEqual(Transaction.objects.filter(user_id=self.user.id).count(), 0)
