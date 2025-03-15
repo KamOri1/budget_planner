7ff2038c9c6805d5963b657c7bf3e38a4244911b
KamOri1 <iknowthis45@gmail.com>
2025-03-15 23:18:40 +0100

    test(BankAccount): created first test for TestBankAccountDeleteView

--- bank_account/tests/test_view.py
+++ bank_account/tests/test_view.py
@@ -9,7 +9,7 @@ from rest_framework.reverse import reverse_lazy
 from users.factories import RandomUserFactory
 
 from ..models import BankAccount
-from ..views import AccountUpdateView
+from ..views import AccountDeleteView, AccountUpdateView
 
 
 class TestBankAccountCreateView(TestCase):
@@ -110,3 +110,46 @@ class TestBankAccountUpdateView(TestCase):
         self.assertEqual(updated_account.name, "Updated  Name Check")
         self.assertEqual(updated_account.number, 111)
         self.assertEqual(updated_account.sum_of_funds, 100)
+
+
+class TestBankAccountDeleteView(TestCase):
+    """
+    Test cases for the BankAccount delete view.
+    These tests verify the behavior of deleting an existing BankAccount, correct redirections,
+    form handling, and database updates
+    """
+
+    def setUp(self):
+        """Set up test data."""
+
+        self.client = Client()
+        self.user1 = RandomUserFactory()
+        self.user2 = RandomUserFactory()
+        self.request = HttpRequest()
+        self.client.force_login(self.user1)
+        self.success_url = reverse_lazy("home_account")
+        self.bank_account1 = BankAccount.objects.create(
+            user=self.user1, name="PKO", number="12345", sum_of_funds=12000
+        )
+        self.bank_account2 = BankAccount.objects.create(
+            user=self.user2, name="STR", number="5555", sum_of_funds=100
+        )
+
+    def test_get_queryset(self):
+        """The test simulates user logging in and checks that only the account he has added are available"""
+
+        users = [self.user1, self.user2]
+
+        for user in users:
+
+            self.request.user = user
+            view = AccountDeleteView()
+            view.request = self.request
+            queryset = view.get_queryset()
+
+            expected_count = BankAccount.objects.filter(user=user).count()
+
+            self.assertEqual(queryset.count(), expected_count)
+
+            for account in queryset:
+                self.assertEqual(account.user, user)
