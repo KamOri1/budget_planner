9c7b568befc071672dae366f4cc5dc23536381fa
KamOri1 <iknowthis45@gmail.com>
2025-03-21 23:15:10 +0100

    test(regular_expenses): created test for created views

--- regular_expenses/tests/test_views.py
+++ regular_expenses/tests/test_views.py
@@ -1,4 +1,9 @@
-from django.test import TestCase
+"""
+Tests for views.
+"""
+
+from django.test import Client, TestCase
+from rest_framework.reverse import reverse_lazy
 
 from category.factories import CategoryFactory
 from users.factories import RandomUserFactory
@@ -6,13 +11,16 @@ from users.factories import RandomUserFactory
 from ..models import RegularExpenses
 
 
-class TestModels(TestCase):
-    """Test RegularExpenses model."""
+class TestRegularExpensesCreateView(TestCase):
+    """Test RegularExpenses create views."""
 
     def setUp(self):
-        """Set up test data."""
+        """Set up test data.Including a logged-in user"""
 
+        self.client = Client()
         self.user = RandomUserFactory()
+        self.client.force_login(self.user)
+        self.index_url = reverse_lazy("create_expenses")
         self.category = CategoryFactory(user=self.user)
         self.regularExpenses = RegularExpenses.objects.create(
             name="Salary",
@@ -22,9 +30,14 @@ class TestModels(TestCase):
             user=self.user,
         )
 
-    def test_model_Category(self):
-        """Test create RegularExpenses"""
+    def test_form_valid(self):
+        """Ensure that the GET request correctly renders the form template."""
 
+        response = self.client.get(self.index_url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "regular_expenses/expenses_add.html")
+
+    def test_post_valid_data_creates_category(self):
         self.assertEqual(str(self.regularExpenses.name), "Salary")
         self.assertEqual(str(self.regularExpenses.category), self.category.name)
         self.assertEqual(str(self.regularExpenses.sum_amount), "122.22")
