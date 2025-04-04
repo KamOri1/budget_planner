afafe4ba1df0a55e4aecd52d204c5b929d4b1636
KamOri1 <iknowthis45@gmail.com>
2025-04-04 23:24:07 +0200

    test(SavingGoal): created TestSavingGoalDeleteVIew

--- saving_goal/tests/test_views.py
+++ saving_goal/tests/test_views.py
@@ -12,7 +12,7 @@ from category.factories import CategoryFactory
 from users.factories import RandomUserFactory
 
 from ..models import SavingGoal
-from ..views import SavingGoalUpdateView
+from ..views import SavingGoalDeleteView, SavingGoalUpdateView
 
 
 class TestSavingGoalCreateView(TestCase):
@@ -136,3 +136,77 @@ class TestSavingGoalUpdateView(TestCase):
         self.assertEqual(updated_goal.target_amount, 345)
         self.assertEqual(updated_goal.target_date, self.test_date)
         self.assertEqual(updated_goal.description, "test description nr 1")
+
+
+class TestSavingGoalDeleteView(TestCase):
+    """
+    Test cases for the goal delete view.
+    These tests verify the behavior of deleting an existing goal, correct redirections,
+    form handling, and database updates
+    """
+
+    def setUp(self):
+        """Set up test data."""
+
+        self.test_date = date(2024, 12, 31)
+        self.success_url = reverse_lazy("goal-home")
+        self.client = Client()
+        self.user1 = RandomUserFactory()
+        self.user2 = RandomUserFactory()
+        self.request = HttpRequest()
+        self.client.force_login(self.user1)
+
+        self.savingGoal1 = SavingGoal.objects.create(
+            name="Vacation1",
+            target_date=self.test_date,
+            target_amount=122.22,
+            description="test description1",
+            user=self.user1,
+        )
+        self.savingGoal2 = SavingGoal.objects.create(
+            name="Vacation2",
+            target_date=self.test_date,
+            target_amount=982.42,
+            description="test description2",
+            user=self.user1,
+        )
+        self.savingGoal3 = SavingGoal.objects.create(
+            name="Vacation3",
+            target_date=self.test_date,
+            target_amount=66.78,
+            description="test description3",
+            user=self.user2,
+        )
+
+    def test_get_queryset(self):
+        """The test simulates user logging in and checks that only the SavingGoal he has added are available"""
+
+        users = [self.user1, self.user2]
+
+        for user in users:
+
+            self.request.user = user
+            view = SavingGoalDeleteView()
+            view.request = self.request
+            queryset = view.get_queryset()
+
+            expected_count = SavingGoal.objects.filter(user=user).count()
+
+            self.assertEqual(queryset.count(), expected_count)
+
+            for goal in queryset:
+                self.assertEqual(goal.user, user)
+
+    def test_delete_goal(self):
+        """Checks whether the selected goal has been deleted."""
+
+        self.client.force_login(self.user1)
+        delete_url = reverse_lazy("delete_goal", args=[self.savingGoal1.pk])
+        response = self.client.post(delete_url)
+
+        self.assertEqual(response.status_code, 302)
+
+        with self.assertRaises(SavingGoal.DoesNotExist):
+            SavingGoal.objects.get(pk=self.savingGoal1.pk)
+
+        self.assertEqual(SavingGoal.objects.filter(user=self.user1.id).count(), 1)
