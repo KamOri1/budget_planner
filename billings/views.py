3c11bc7ebe56d66b16a19fee3754004689570805
KamOri1 <iknowthis45@gmail.com>
2025-07-07 22:43:31 +0200

    feat(billing): add context data for template

--- billings/views.py
+++ billings/views.py
@@ -1,13 +1,14 @@
-from django.shortcuts import render
 from django.views.generic import TemplateView
 
-
-def billing(request):
-    return render(
-        request=request,
-        template_name="billings/billing_home_page.html",
-    )
+from dashboard.utils.today_money import TodayMoney
 
 
 class BillingView(TemplateView):
     template_name = "billings/billing_home_page.html"
+
+    def get_context_data(self, **kwargs):
+        today_profit = TodayMoney(self.request.user)
+        context = {
+            "today_profit": today_profit.today_profit,
+        }
+        return context
--- transaction/filters.py
+++ transaction/filters.py
@@ -5,7 +5,9 @@ from .models import Transaction
 
 class TransactionFilter(django_filters.FilterSet):
     transaction_name = django_filters.CharFilter(lookup_expr="iexact")
-    transaction_date = django_filters.DateFilter(field_name="date", lookup_expr="gte")
+    transaction_date = django_filters.DateFilter(
+        field_name="transaction_date", lookup_expr="exact"
+    )
     sum_amount = django_filters.NumberFilter(field_name="sum_amount", lookup_expr="gte")
 
     class Meta:
