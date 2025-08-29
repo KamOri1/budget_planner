7a8e98d380f5794686881b18d582aad0f1f4fdff
KamOri1 <iknowthis45@gmail.com>
2025-08-30 00:30:16 +0200

    feat(billings): prepere transaction list view

--- billings/views.py
+++ billings/views.py
@@ -1,6 +1,10 @@
 from django.views.generic import TemplateView
 
+from datetime import date
+from datetime import timedelta
+
 from dashboard.utils.today_money import TodayMoney
+from transaction.models import Transaction
 
 from .utils.possesion_values import PossessionValues
 
@@ -10,9 +14,13 @@ class BillingView(TemplateView):
 
     def get_context_data(self, **kwargs):
         today_profit = TodayMoney(self.request.user)
+        today = date.today()
+        yesterday = today - timedelta(days=1)
         context = {
             "today_profit": today_profit.today_profit,
             "possession_values": PossessionValues(self.request.user).calculate(),
+            "transactions_today": Transaction.objects.filter(transaction_date__date=today),
+            "transactions_yesterday": Transaction.objects.filter(transaction_date__date=yesterday),
         }
         print(context)
         return context
